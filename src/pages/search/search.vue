ecf125664b8bcab420f965a670f8ed6ae615ed6d
hcyhehe <960696163@qq.com>
2019-12-25 00:28:32 +0800

    bbbbbbb

--- src/pages/search/search.vue
+++ src/pages/search/search.vue
@@ -3,25 +3,35 @@
     <div class="s1">
       <van-search
         v-model="keywords"
-        placeholder="请输入搜索关键词"
+        placeholder="请输入歌曲关键词"
         show-action
         @search="onSearch"
         @cancel="onCancel"
       />
     </div>
     <div class="s2">
-
+      <van-tabs v-model="active" color="#aaa" title-active-color="#000" title-inactive-color="#666">
+        <van-tab title="网易云音乐"></van-tab>
+        <van-tab title="QQ音乐"></van-tab>
+      </van-tabs>
     </div>
-    <div class="s3">
+    <div class="s3" v-if="list.length>0">
       <div class="one" v-for="(obj,index) in list" :key="index">
-        <div class="o1">{{index+1}}</div> 
+        <div class="o1" v-show="Index!==index">{{index+1}}.</div>
+        <img class="o5" v-show="Index===index" src="../../assets/img/wave.gif" />
         <div class="o2">{{obj.name}}</div> 
-        <div class="o3" @click="player(obj.id)"><van-icon name="play-circle-o" size="0.85rem" /></div>
+        <div class="o3" @click="player(obj.id, index)">
+          <van-icon v-show="Index!==index" name="play-circle-o" size="0.85rem" />
+          <van-icon v-show="Index===index" name="pause-circle-o" size="0.85rem" />
+        </div>
         <div class="o4">{{obj.artists[0].name}}</div>
       </div>
     </div>
+    <div class="s3" v-if="list.length==0">
+      <div class="text">欢迎使用“随搜听”音乐播放器</div>
+    </div>
 
-    <audio id="audio" :src="href" autoplay="autoplay" loop="loop"></audio>
+    <audio id="audio" class="s4" :src="href" loop="loop" controls></audio>
   </div>
 </template>
 
@@ -39,8 +49,9 @@ export default {
     return{
       keywords: '',
       list: [],
-      status: false,
       href: '',
+      Index: '',
+      active: 0,
     }
   },
   methods: {
@@ -61,9 +72,19 @@ export default {
     onCancel(){
       this.keywords = ''
     },
-    player(id){
-      this.href = 'https://music.163.com/song/media/outer/url?id='+id+'.mp3'
-      console.log(this.href)
+    player(id, index){
+      let audio = document.getElementById('audio')
+      if(this.Index !== index){
+        this.Index = index
+        this.href = 'https://music.163.com/song/media/outer/url?id='+id+'.mp3'
+        console.log(this.href)
+        setTimeout(()=>{
+          audio.play()
+        }, 100)
+      } else {
+        this.Index = ''
+        audio.pause()
+      }
     },
   },
 
@@ -72,13 +93,8 @@ export default {
   },
 
   mounted(){
-    let that = this;
-    
-  },
-
-  beforeDestroy(){
-    
-  },
+    //this.height = document.body.clientHeight + 'px'
+  }
 }
 </script>
 
@@ -87,7 +103,7 @@ export default {
 html,body{
   width: 100%;
   height: 100%;
-  background: #868189;
+  background: #b2b2b2;
 }
 
 .search {
@@ -100,11 +116,19 @@ html,body{
     z-index: 10;
   }
   .s2{
-    margin-top: 1.45rem;
+    margin-top: 1.31rem;
   }
   .s3{
-    width: 90%;
+    width: 92%;
     margin: 0 auto;
+    padding-bottom: 1.28rem;
+    .text{
+      height: 2rem;
+      line-height: 2.5rem;
+      color: #fff;
+      text-align: center;
+      font-size: 0.43rem;
+    }
     .one{
       display: flex;
       height: 1.2rem;
@@ -113,28 +137,44 @@ html,body{
       border-bottom: 1px solid hsla(0,0%,100%,.1);
       color: #fff;
       .o1{
-        width: 5%;
+        width: 8%;
+        text-align: center;
       }
       .o2{
-        width: 54%;
+        width: 55%;
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
       }
       .o3{
-        width: 18%;
+        width: 15%;
         text-align: center;
         i{
           margin-top: 0.13rem;
         }
       }
       .o4{
-        width: 23%;
+        width: 22%;
         overflow: hidden;
         text-overflow: ellipsis;
         white-space: nowrap;
       }
+      .o5{
+        width: 4%;
+        height: 0.5rem;
+        margin-left: 2%;
+        margin-right: 2%;
+        margin-top: 0.3rem;
+      }
     }
+  }
+  .s4{
+    position: fixed;
+    width: 100%;
+    height: 1.2rem;
+    left: 0;
+    bottom: 0;
+    z-index: 10;
   }
 }
 </style>
